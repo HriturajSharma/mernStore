fd1a1818722255c7837f80ff135df7711fdb04cf
hrituraj <sharmahrituraj2003@gmail.com>
2026-08-23 22:11:39 +0530

    changes in multiple files

--- app/server/Routes/Register.js
+++ app/server/Routes/Register.js
@@ -1,13 +1,13 @@
 import express from "express";
 // import login from '../controllers/login.js';
 // import register from "../controllers/Register.js";
-import { login, register, resetPassword } from "../controllers/Register.js";
+import { forgetPassword, login, register, resetPassword } from "../controllers/Register.js";
 let router = express.Router()
 
 
 router.post("/register",register)
 router.post("/login",login)
-// router.post("/forget-password",forgetPassword)
+router.post("/forget-password",forgetPassword)
 router.post("/reset-password/:token",resetPassword)
 
 
